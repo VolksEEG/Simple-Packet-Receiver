fe098305dd7df0a6399115593148f8c07d0e9a6b
alcohen <amc79@cornell.edu>
2022-08-26 21:07:44 -0400

    Add project files.

---
+++ Simple Packet Receiver/frmMain.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace Simple_Packet_Receiver
+{
+    partial class frmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnAcquire = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbPort = new System.Windows.Forms.ComboBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnAcquire
+            // 
+            this.btnAcquire.BackColor = System.Drawing.SystemColors.Control;
+            this.btnAcquire.Location = new System.Drawing.Point(12, 22);
+            this.btnAcquire.Name = "btnAcquire";
+            this.btnAcquire.Size = new System.Drawing.Size(131, 57);
+            this.btnAcquire.TabIndex = 0;
+            this.btnAcquire.Text = "Acquire";
+            this.btnAcquire.UseVisualStyleBackColor = false;
+            this.btnAcquire.Click += new System.EventHandler(this.btnAcquire_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutput.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.txtOutput.ForeColor = System.Drawing.Color.LimeGreen;
+            this.txtOutput.Location = new System.Drawing.Point(12, 100);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtOutput.Size = new System.Drawing.Size(776, 338);
+            this.txtOutput.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(249, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 30);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Port";
+            // 
+            // cmbPort
+            // 
+            this.cmbPort.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbPort.FormattingEnabled = true;
+            this.cmbPort.Location = new System.Drawing.Point(305, 32);
+            this.cmbPort.Name = "cmbPort";
+            this.cmbPort.Size = new System.Drawing.Size(166, 38);
+            this.cmbPort.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmbPort);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnAcquire);
+            this.Name = "frmMain";
+            this.Text = "SImple Packet Receiver";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAcquire;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbPort;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Simple Packet Receiver/frmMain.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SimplePacketLib;
+using System.Diagnostics;
+
+namespace Simple_Packet_Receiver
+{
+    public partial class frmMain : Form
+    {
+        SerialPort serialPort = new SerialPort(); // = new SerialPort();
+        private int numDisplayChans = 8;
+        private int numPacketsReceived = 0;
+        //SimplePacket currPacket = new SimplePacket(;
+        private SimplePacketParser packetizer = new SimplePacketParser();
+        private SimplePacket packetBuffer = new SimplePacket();
+        private Stopwatch stopwatch = new Stopwatch();
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            serialPort.DataReceived += new SerialDataReceivedEventHandler(SerialReceivedHandler);
+            
+            //get com ports
+            var ports = SerialPort.GetPortNames();
+            cmbPort.DataSource = ports;
+
+            if (ports.Contains<String>(Properties.Settings.Default.SelectedPort))
+            {
+                cmbPort.Text = Properties.Settings.Default.SelectedPort;
+            }
+        }
+
+        private void btnAcquire_Click(object sender, EventArgs e)
+        {
+            serialPort.PortName = cmbPort.Text;
+            serialPort.BaudRate = 921600;
+            serialPort.Parity = Parity.None;
+            serialPort.DataBits = 8;
+            serialPort.StopBits = StopBits.One;
+            serialPort.Open();
+            //timer1.Enabled = true;
+            stopwatch.Start();
+        }
+
+        private void SerialReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            //string indata = sp.ReadExisting();
+            for (int i = 0; i < sp.BytesToRead; i++)
+            {
+                byte b = Convert.ToByte(sp.ReadByte());
+                bool newPacket = this.packetizer.AddByte(b, packetBuffer);
+                if (newPacket)
+                {
+                    this.Invoke((MethodInvoker)delegate { txtOutput.AppendText(Environment.NewLine); });
+                    this.Invoke((MethodInvoker)delegate { txtOutput.AppendText(packetBuffer.StartFlag + " " + packetBuffer.Counter); });
+                    for (int j = 0; j < packetBuffer.ChannelSet.Length; j++)
+                    {
+                        this.Invoke((MethodInvoker)delegate { txtOutput.AppendText(" " + packetBuffer.ChannelSet[j]); });
+                    }
+                    numPacketsReceived++;
+                }
+            }            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (stopwatch.ElapsedMilliseconds > 1000)
+            {
+                serialPort.DataReceived -= SerialReceivedHandler;
+                stopwatch.Reset();
+            }
+        }
+    }
+}
